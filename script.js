dd4990383e68b7e43eb78596a085108f865d3a49
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-23 02:25:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+	const firstNameInput= document.querySelector('input[name="fname"]');
+	const lastNameInput= document.querySelector('input [name="lname"]');
 
+	const fname=firstNameInput.value;
+	const lname= lastNameInput.value;
+
+	const fullName = '${fname} ${lname}';
+	alert('${fullName}');
+	
 }
+
