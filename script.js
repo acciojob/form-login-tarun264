0b67ccb8929f05a87981352f0a9e8f4b0e300ce4
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-10-03 00:58:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function getFormvalue(event) {
 	event.preventDefault();
-	const firstNameInput= document.getElementById('fname').value;
-	const lastNameInput= document.getElementById('lname').value;
+	
+	let x= document.getElementById("form1");
+	let name="";
+	for(let i=0;i<x.length;i++){
+		if(x.element[i].value!='Submit'){
+			name+=x.element[i].value+" ";
+		}
+	}
 
-	alert( firstNameInput+""+lastNameInput);	
+	alert(name.substring(0,name.length-1));	
 }
 
